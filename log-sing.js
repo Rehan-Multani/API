95b480e97775914d71df0cca23ec4a15ac547dc6
Rehan-Multani <mohammadrehan00121@gmail.com>
2024-08-07 17:29:18 +0530

    d

--- log-sing.js
+++ log-sing.js
@@ -6,9 +6,11 @@ const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
 
-const User = mongoose.model('User', new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+const User = mongoose.model('myusers', new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true }
+
 }));
 
 mongoose.connect('mongodb+srv://newDoctor:tND1w2lLKDE8oA2B@cluster0.oglqeqt.mongodb.net/NewDoctor', {
@@ -19,17 +21,17 @@ mongoose.connect('mongodb+srv://newDoctor:tND1w2lLKDE8oA2B@cluster0.oglqeqt.mong
 
 app.post('/signup', async (req, res) => {
     const { name,email, password } = req.body;
-    const user = await User.findOne({ name });
+    const user = await User.findOne({ email });
     if (user) return res.status(400).send('User already registered.');
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({ name, email,password: hashedPassword });
-    await newUser.save();
+    const result=await newUser.save();
 
     const token = jwt.sign({ _id: newUser._id }, 'herjbhfiurbfrbnf');
-    res.send(token);
+    res.send(result);
 });
 
 app.post('/login', async (req, res) => {
